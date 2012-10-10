ed6f10d38cd9be74a2fab838ec7831503f7dcff6
Gerry Miller <gerry@museholdings.com>
2012-10-10 09:44:37 -0400

    Initial commit

---
+++ App_Start/BundleConfig.cs
@@ -0,0 +1,21 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace HTML5Boilerplate
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/normalize.min.css",
+                "~/Content/main.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include("~/Scripts/libs/modernizr-2.6.1.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+                "~/Scripts/plugins.js",
+                "~/Scripts/main.js"));
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HTML5Boilerplate.MvcApplication" Language="C#" %>
